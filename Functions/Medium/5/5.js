0da67e992bf8d9610e754bcd2923e12ec01a127e
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2022-11-14 23:29:34 +0530

    initial commit

---
+++ Functions/Medium/5/5.js
@@ -0,0 +1,9 @@
+// Given a string and an index, your function should return the character present at that index in the string.
+// Example:
+// Input: charAt("neoGcamp", 4) ––> Output: c
+
+let str="neoGcamp";
+let index=4;
+
+console.log(str[index]);
+
