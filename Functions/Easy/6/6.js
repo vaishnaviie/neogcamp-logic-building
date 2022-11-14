72960385a6400ba898dab2e762b27dd128b86f22
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2022-11-14 23:15:12 +0530

    initial commit

---
+++ Functions/Easy/6/6.js
@@ -0,0 +1,3 @@
+// Given three angles of a triange, your function should return if it is a scalene, isosceles, equilateral triangle or not a triangle at all. Example:
+// Input: typeOfTriangle(30, 60, 90) ––> Output: Scalene Triangle
+
---
+++ Functions/Medium/1/1.js
@@ -0,0 +1,10 @@
+// Given an array, your function should return the length of the array.
+// Example:
+// Input: arrayLength([1,5,3,7,8]) ––> Output: 5
+
+const arr=[1,5,3,7,8];
+let size=0;
+for(let i=0;i<arr.length;i++){
+    size++;
+}
+console.log(size);
