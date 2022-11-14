dac36dc6e87f2453db7d64ffc2791241006d0533
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2022-11-14 23:08:36 +0530

    initial commit

--- Functions/Easy/4/4.js
+++ Functions/Easy/4/4.js
@@ -3,3 +3,12 @@
 // Input: findMin(3,5) ––> Output: 3
 // Input: findMin(3,5,9,1) ––> Output: 1
 // (Hint: Lookup rest parameters in JavaScript)
+
+const arr=[3,5,9,1];
+let ans=100000;
+for(let i=0;i<arr.length;i++){
+    if(ans>arr[i]){
+        ans=arr[i];
+    }
+}
+console.log(ans);
